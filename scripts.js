045b2f463b821fe4c6b14c10ad9103c85d865f5e
Alejandro Bertinelli <alexberti02@gmail.com>
2019-04-27 14:22:16 +0200

    Mejoras de estilo

--- scripts.js
+++ scripts.js
@@ -4,9 +4,4 @@ function funcProg() {
 	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
 	if(winScroll > 0) document.getElementsByTagName("header").item(0).classList.add('header2');
 	else document.getElementsByTagName("header").item(0).classList.remove('header2');
-
-	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-	var scrolled = (winScroll / height) * 100;
-	var barra = document.getElementById("progbar");
-	if(barra)barra.style.width = scrolled + "%";
 }
